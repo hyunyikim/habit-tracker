9712585aeb9bab8627a3eb3adadd6d5b8fc6bf83
hyunyikim <kim5518200@naver.com>
2021-10-17 22:26:47 +0900

    Add react configuration

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="container">Container</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    hot: true,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
